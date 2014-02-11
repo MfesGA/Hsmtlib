8e6df7d82573d840e1f2d19777bac1ecc6ecacee
Nuno <numicola@gmail.com>
2014-02-11 17:57:30 +0000

    add the possibility of saving and loading a config from a file

--- SmtConfig.hs
+++ SmtConfig.hs
@@ -12,9 +12,6 @@ data Config = Config
             , avaliable_modes :: [String]
             }deriving(Show)
 
-
-
-
 instance FromJSON Config where
   parseJSON (Object v) = 
                         Config <$> v .: "path"
@@ -31,12 +28,54 @@ instance ToJSON Config where
           ,  "avaliable_modes" .= avaliable_modes
           ]
           
+-- |this fuction will give back a config with the parms in the file specified in path
+getconfig :: String -> IO( Either String Config)
+getconfig path = do 
+	    file <- B.readFile path
+	    return $ eitherDecode file
+
+-- | this function will give a new config when present a old config and a new path
+chPath :: Config -> String -> Config
+chPath c path_n=  Config {
+					path= path_n,
+					args= args c,
+					default_mode=default_mode c,
+					avaliable_modes= avaliable_modes c}
+-- | this function will give a new config when present a old config and a new ARGS
+chArgs :: Config -> [String] -> Config
+chArgs c args_n=  Config {
+					path= path c,
+					args= args_n,
+					default_mode=default_mode c,
+					avaliable_modes= avaliable_modes c}
+-- | this function will give a new config when present a old config and a new Default_mode
+chDefaultMode :: Config -> String -> Config
+chDefaultMode c default_mode_n=  Config {
+					path= path c,
+					args= args c,
+					default_mode=default_mode_n,
+					avaliable_modes= avaliable_modes c}
+-- | this function will give a new config when present a old config and a new available modes
+chAvailableModes :: Config -> [String] -> Config
+chAvailableModes c available_modes_n=  Config {
+					path= path c,
+					args= args c,
+					default_mode=default_mode c,
+					avaliable_modes= available_modes_n}
 
 
-main :: IO ()
-main  = do
-  file <- B.readFile "solvers_config/cvc4.json" :: IO B.ByteString
-  case (decode file) of
-    Nothing -> print "Nada"
-    Just x -> print (x::Config)
-  
+-- | this function will create a config with the paramethers given
+genconfig :: (String, [String], String, [String]) -> Config
+genconfig (path_n, args_n, default_mode_n, avaliable_modes_n)= Config {
+								path= path_n,
+								args= args_n,
+								default_mode=default_mode_n,
+								avaliable_modes= avaliable_modes_n}
+
+-- | this function will create and put a config in a file specified in path 
+generateconfig :: (String, [String], String, [String]) -> String -> IO()
+generateconfig config_parms file_name= do
+								B.writeFile file_name $ encode $ genconfig config_parms
+								 
+
+
